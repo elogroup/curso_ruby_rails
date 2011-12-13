1562e0cdae722e9d9ecfce3b840db43603a39b15
Sergio Junior <sergior.jr@gmail.com>
2011-12-13 17:35:00 -0200

    exemplo de blocos (bloco.rb) e refactor da busca pra usar blocos

--- banco/lib/banco.rb
+++ banco/lib/banco.rb
@@ -10,15 +10,34 @@ class Banco
   end
   
   def localiza_conta_por_nome(nome)
-    for conta in @contas
-      return conta if conta.titular == nome
-    end
+    localiza_conta{|c| c.titular == nome }
   end
   
   def localiza_conta_por_numero(numero)
-    for conta in @contas
-      return conta if conta.numero == numero
-    end    
+    localiza_conta{|c| c.numero == numero }      
   end
   
+  private
+    def localiza_conta(&criterio)
+      for conta in @contas
+        if yield(conta)
+          return conta
+        end
+      end
+      nil
+    end
+  
+  
 end
+
+
+
+
+
+
+
+
+
+
+
+
