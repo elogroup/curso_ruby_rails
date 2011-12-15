c04ba065be5e0839dce9cddee18ae275b126b876
Sergio Junior <sergior.jr@gmail.com>
2011-12-15 15:10:28 -0200

    incluindo mensagens flash

--- todo/app/controllers/tarefas_controller.rb
+++ todo/app/controllers/tarefas_controller.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 class TarefasController < ApplicationController
   
   def novo
@@ -7,8 +8,10 @@ class TarefasController < ApplicationController
   def create
     @tarefa = Tarefa.new params[:tarefa]
     if !@tarefa.save
+      flash[:error] = "Existem erros que impedem a inclusão"
       render :action => :novo
     else
+      flash[:notice] = "Tarefa #{params[:id]} criada com sucesso"
       redirect_to :action => :index
     end
   end
@@ -25,14 +28,17 @@ class TarefasController < ApplicationController
     @tarefa = Tarefa.find params[:id]
     @tarefa.update_attributes params[:tarefa]
     if @tarefa.save
+      flash[:notice] = "Tarefa #{params[:id]} atualizada com sucesso"
       redirect_to :action => :index
     else
+      flash[:error] = "Existem erros que impedem a atualizacao"
       render :action => :edit
     end
   end
   
   def destroy
     Tarefa.destroy params[:id]
+    flash[:notice] = "Tarefa #{params[:id]} removida com sucesso"
     redirect_to :action => :index
   end
   
