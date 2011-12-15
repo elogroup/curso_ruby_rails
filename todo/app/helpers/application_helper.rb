d267de865a42e0b67cd48a116b25e47a4f089c4b
Sergio Junior <sergior.jr@gmail.com>
2011-12-15 15:34:24 -0200

    helper para fazer menu principal

--- todo/app/helpers/application_helper.rb
+++ todo/app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  
+  def menu_principal
+    opcoes = %w(tarefas projetos colaboradores)
+    html = "<div id='menu'>"
+    html << '<ul>'
+    opcoes.each do |op|
+      if respond_to?("#{op}_index_path")
+        link = link_to op.capitalize, send("#{op}_index_path")
+      else
+        link = link_to op.capitalize, '#'
+      end
+      html << "<li style='display:inline'>#{link} | </li>"
+    end
+    html << '</ul>'
+    html << '</div>'
+
+    raw html
+  end
 end
