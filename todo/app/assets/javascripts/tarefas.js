02c3ac87a8e5cfbdf27f782cfef0f8f44a767ba1
Sergio Junior <sergior.jr@gmail.com>
2011-12-16 15:35:15 -0200

    ajax de remover tarefa completo com funcao js para remover elementos

--- todo/app/assets/javascripts/tarefas.js
+++ todo/app/assets/javascripts/tarefas.js
@@ -1,7 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-
-$('.remove-tarefa').bind('ajax:success', function(){
-	var elemento_id = $(this).data('elemento_id');
-	$('#'+elemento_id).remove();
-});
+$(function(){
+	$('.remove-tarefa').bind('ajax:success', function(){
+		var elemento_id = $(this).data('elemento_id');
+		$('#'+elemento_id).remove();
+	});	
+})
