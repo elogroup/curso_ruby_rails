60813ce1c993c42755670b9a74de99f3a0133ba3
Sergio Junior <sergior.jr@gmail.com>
2012-11-22 14:01:18 -0200

    incluindo banco e spec do banco

---
+++ banco/spec/banco_spec.rb
@@ -0,0 +1,10 @@
+#encoding: utf-8
+require '../lib/banco'
+
+describe Banco do
+  it "deve ter contas"
+
+  it "deve se possivel incluir uma conta"
+
+  it "deve ser possivel remover uma conta"
+end
